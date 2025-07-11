75f33d74fb593cc6a252ea62e828e9706f8fda44
lihe <lihe@jiayinfintech.cn>
2025-07-11 18:09:00 +0800

    更新

--- src/pages/legal/app/index.vue
+++ src/pages/legal/app/index.vue
@@ -3,12 +3,16 @@
     <div class="banner">
         
     </div>
-    <div class="mod">
-        <div>In real life, legal problems are everywhere:</div>
-        <div>Have you ever suffered from unclear contract content?</div>
-        <div>What is Divorce and Property Sharing?</div>
-        <div>Is there a dispute over workplace injustice or dismissal?</div>
-        <div>Is there a dispute over workplace injustice or dismissal? Or are you a small business owner who wants to understand business laws and compliance risks?</div>
+    <div class="mod why">
+        <div class="title"></div>
+        <div class="subTitle"><span></span><p>In real life, legal problems are everywhere:</p></div>
+        <div class="text"><span></span><p>Have you ever suffered from unclear contract content?</p></div>
+        <div  class="text"><span></span><p>What is Divorce and Property Sharing?</p></div>
+        <div  class="text"><span></span><p>Is there a dispute over workplace injustice or dismissal?</p></div>
+        <div class="text"><span></span><p>Is there a dispute over workplace injustice or dismissal? Or are you a small business owner who wants to understand business laws and compliance risks?</p></div>
+        <div class="reminder">
+            Whatever your situation, LawOnGo offers fast, reliable and affordable legal consulting services. We are committed to making “please lawyer” no longer a luxury option, but a right that everyone has.
+        </div>
     </div>
   </div>
 </template>
