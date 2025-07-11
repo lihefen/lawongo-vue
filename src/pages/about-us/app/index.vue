d797be5a0545d1e7348452b07a0ae8cc6bd0022d
lihe <lihe@jiayinfintech.cn>
2025-07-11 10:14:59 +0800

    关于我们

---
+++ src/pages/about-us/app/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-page">
+    <common-header title="首页" />
+    <div class="content">
+      <van-cell-group>
+        <van-cell title="Vue 2.7 + Vant" value="多页面应用" />
+        <van-cell title="技术栈" value="Sass + vw适配" />
+      </van-cell-group>
+      <van-button type="primary" block @click="showToast">点击测试</van-button>
+    </div>
+  </div>
+</template>
+
+<script src="./index.js"></script>
+<style lang="scss" src="./index_module.scss" scoped></style>
