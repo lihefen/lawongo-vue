d65f90859563a5d786fd7147b5af03fab5272da5
lihe <lihe@jiayinfintech.cn>
2025-07-11 17:07:02 +0800

    postcss修改

---
+++ src/pages/legal/app/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <div class="banner">
+        
+    </div>
+    <div class="mod">
+        <div>In real life, legal problems are everywhere:</div>
+        <div>Have you ever suffered from unclear contract content?</div>
+        <div>What is Divorce and Property Sharing?</div>
+        <div>Is there a dispute over workplace injustice or dismissal?</div>
+        <div>Is there a dispute over workplace injustice or dismissal? Or are you a small business owner who wants to understand business laws and compliance risks?</div>
+    </div>
+  </div>
+</template>
+
+<script src="./index.js"></script>
+<style lang="scss" src="./index_module.scss" scoped></style>
