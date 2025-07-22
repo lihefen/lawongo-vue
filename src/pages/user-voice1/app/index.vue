dd228adcd58e9ded428c7cff852d4afcb78d4b5a
lihe <lihe@jiayinfintech.cn>
2025-07-22 14:20:17 +0800

    用户之声

---
+++ src/pages/user-voice1/app/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="main">
+        <!-- <common-header title="Konsultasi Telepon Cepat" :showBack="true" backPath="/" /> -->
+        <div class="banner">
+        </div>
+        <div class="headerBanner">
+
+        </div>
+        <div class="mod">
+            <div class="title"></div>
+            <div class="list">
+                <div class="item">1.lsi data untuk semua platform dalam 3 menit</div>
+
+                <div class="item">2.Penilaian cerdas dalam 2 jam</div>
+
+                <div class="item">2.Negosiasi selesai dalam 7 hari</div>
+            </div>
+            
+            <div class="correct">
+                <span></span>Reputasi profesional terjaga penuh
+            </div>
+
+            <div class="correct">
+                <span></span>Enkripsi ketat 100% - data hanya untuk mediasi
+            </div>
+        </div>
+    </div>
+</template>
+
+<script src="./index.js"></script>
+<style lang="scss" src="./index_module.scss" scoped></style>
