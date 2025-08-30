8060ffec0025a62efd4f83ed57760efa6901fe6f
lihe <lihe@jiayinfintech.cn>
2025-08-30 11:44:52 +0800

    law

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -56,7 +56,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: '[name]/js/[name].[contenthash:8].js', // 按页面名分组
-    publicPath: '/h5/',
+    // publicPath: '/h5/',
+    publicPath: '/',
     clean: true, // Clean the output directory before emit.
   },
   resolve: {
